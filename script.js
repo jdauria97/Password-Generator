560fb5765a61e12256b1a5ed48a2279cc0207332
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2023-11-27 18:36:58 -0500

    adds comments to script and blank readme

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 
-
+// establishes arrays for each of the character types 
 var criteria = {
     uppercaseABC: "ABCDEFGHIJKLMNOPQRSTUVWXZ",
     lowercaseABC: "abcdefghijklmnopqrstuvwxyz",
@@ -13,19 +13,25 @@ var criteria = {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// selects random characters from the arrays 
 var getRandomChar = function (fromString) {
     return fromString[Math.floor(Math.random() * fromString.length)];
 };
 
+// overall function for the entire process 
 function generatePassword() {
+    // window prompts to select password criteria 
     var chooseLength = window.prompt("Select password length from '8'-'128' characters");
     var chooseUpper = window.confirm("Do you want uppercase letters?");
     var chooseLower = window.confirm("Do you want lower case letters?");
     var chooseSymb = window.confirm("Do you want special characters?");
     var chooseNumb = window.confirm("Do you want numbers?");
+    // empty array that will form password criteria choices
     var info = "";
+    // empty array that will contain the selected characters based upon chosen criteria 
     var characters = [];
 
+    // boolean statements select character criteria choice 
     if (chooseLength >= 8 && chooseLength <= 128) {
         if (chooseUpper) {
             info += criteria.uppercaseABC;
@@ -50,6 +56,7 @@ function generatePassword() {
             window.alert("You must choose at least one criteria option. please try again.");
             generatePassword();
         };
+        // limits array of password characters to chosen length and then randomly shuffles 
         for (let i = characters.length - 1; i > 0; i--) {
             var shuffle = Math.floor(Math.random() * (i + 1));
             var temp = characters[i];
